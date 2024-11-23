2e07e03b6ec7fc9068a591a8bd8319f56a42889d
laurabauk <laura7bauk@gmail.com>
2024-11-23 01:35:34 +0100

    initial commit with visual studio

---
+++ Project App/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ProjectManager.Classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+
+    class Project
+    {
+        public string Naziv { get; set; }
+        public string Opis { get; set; }
+        public DateTime DatumPocetka { get; set; }
+        public DateTime DatumZavrsetka { get; set; }
+        public string Status { get; set; }
+    }
+
+    class Task
+    {
+        public string Naziv { get; set; }
+        public string Opis { get; set; }
+        public DateTime RokIzvrsenja { get; set; }
+        public string Status { get; set; }
+        public int OcekivanoVrijemeTrajanja { get; set; }
+    }
+}
+
